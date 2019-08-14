6831bbbefd434ffd39abb103480e475427c6cf6e
楼勇 <louyong@lianluo.com>
2019-08-14 16:02:57 +0800

    Add lianxi mode.

---
+++ static/js/home.js
@@ -0,0 +1,12 @@
+var button1 = $("#button1");
+var button2 = $("#button2");
+
+button1.click(function () {
+    console.log('button1 clicked！');
+    window.location='./weiqi.html';
+})
+
+button2.click(function () {
+    console.log('button2 clicked！');
+    window.location='./lianxi.html';
+})
